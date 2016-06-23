91037e04c757f2dc1cc8838e3d7c3efbe7793c55
Gao Yuhao <gaoyuhao@outlook.com>
2016-06-23 09:07:48 +0800

    规范编码格式
    规范编码格式，取消os包调用

--- SpiderWE+.py
+++ SpiderWE+.py
@@ -1,31 +1,28 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'爬虫实例(需登录)'
+
+__author__ = 'Gao Yuhao'
+
 import re
 import urllib
 import urllib2
 import sqlite3
-import os
-#import xlrd
-import sqlite3
 import cookielib
 
 # 打开数据库文件
- 
-WE_db = sqlite3.connect(r'.\WE+.db')
+WE_db = sqlite3.connect(r'./WE+.db')
 cursor = WE_db.cursor()
  
 # 建表
- 
 cursor.execute('DROP TABLE IF EXISTS loanlog')
 cursor.execute('CREATE TABLE loanlog (loanid int primary key, userid int, user_name varchar(50), age int, address varchar(20), graduation varchar(20),marriage varchar(20), job_type varchar(50), income varchar(100), success int, overdue int, risk_level varchar(8), borrowtype varchar(50), amount varchar(10), months int, interest varchar(8),  finish_ratio varchar(8))')
  
 #定义正则表达式匹配规则
-
 pattern = re.compile(r'"user":(?P<userid>\d+).*"loanId":(?P<loanid>\d+).+"borrowType":"(?P<borrowtype>[^"]+).+"amount":(?P<amount>[^,]+),"interest":(?P<interest>[0-9.]+),"months":(?P<months>\d+).+"finishedRatio":(?P<finish_ratio>[^,]+).+"nickName":"(?P<user_name>[^"]+).+"borrowerLevel":"(?P<risk_level>[^"]+)","address":"(?P<address>[^"]*)","jobType":"(?P<job_type>[^"]*)".+\xe5\xb9\xb4\xe9\xbe\x84</span><em>(?P<age>\d+)</em>.*\xe5\xad\xa6\xe5\x8e\x86</span><em>(?P<graduation>.+)</em>.*\xe5\xa9\x9a\xe5\xa7\xbb</span><em>  (?P<marriage>.+\xe5\xa9\x9a).*\xe6\x88\x90\xe5\x8a\x9f\xe5\x80\x9f\xe6\xac\xbe</span><em>(?P<success>\d+).*\xe9\x80\xbe\xe6\x9c\x9f\xe6\xac\xa1\xe6\x95\xb0</span><em>(?P<overdue>\d+).*\xe6\x94\xb6\xe5\x85\xa5</span><em>(?P<income>.+\xe5\x85\x83)</em></div></td>\n',re.S|re.M) 
 
 #定义爬虫函数
-     
 def parse(url):
     req = opener.open(url) 
     try:
@@ -42,7 +39,6 @@ page_start = input('Please input the number you want to start:')
 page_end = input('Then input the number you want to stop:')+1
 
 #初始化cookiejar
-
 cookie = cookielib.CookieJar()  
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))  
 postdata=urllib.urlencode({    
@@ -56,11 +52,9 @@ req = urllib2.Request(
 )  
  
 #登录账号
-
 result = opener.open(req)  
 
 #导入数据库
-
 index = 0
 try:
     for page_index in range (page_start,page_end):
@@ -81,5 +75,7 @@ try:
 except:
     print 'there is an error at'+sName
 
-
-os.system("pause")
+try:
+    input(u'按回车键退出')
+except:
+    pass
--- SpiderWE.py
+++ SpiderWE.py
@@ -1,56 +1,68 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'爬虫实例(需登录)'
+
+__author__ = 'Gao Yuhao'
+
 import re
 import urllib2
 import sqlite3
-import os
-#import xlrd
-import sqlite3
- 
+
 # 打开数据库文件
 WE_db = sqlite3.connect(r'.\WE.db')
 cursor = WE_db.cursor()
- 
+
 # 建表
 cursor.execute('DROP TABLE IF EXISTS loanlog')
-cursor.execute('CREATE TABLE loanlog (loanid int primary key,user_name varchar(50), address varchar(50), job_type varchar(100), amount varchar(10), interest varchar(6), months varchar(3), risk_level varchar(3), finish_ratio varchar(6))')
- 
-#正则匹配
-pattern = re.compile(r'"loanId":(?P<loanid>\d+).+"amount":(?P<amount>[^,]+),"interest":(?P<interest>[0-9.]+),"months":(?P<months>\d+).*"finishedRatio":(?P<finish_ratio>[^,]+).*"nickName":"(?P<user_name>[^"]+).+"borrowerLevel":"(?P<risk_level>[^"]+)","address":"(?P<address>[^"]*)","jobType":"(?P<job_type>[^"]*)"',re.S|re.M) 
+cursor.execute(
+    'CREATE TABLE loanlog (loanid INT PRIMARY KEY,user_name VARCHAR(50), address VARCHAR(50), job_type VARCHAR(100), amount VARCHAR(10), interest VARCHAR(6), months VARCHAR(3), risk_level VARCHAR(3), finish_ratio VARCHAR(6))')
 
-#定义爬虫函数  
+# 正则匹配
+pattern = re.compile(
+    r'"loanId":(?P<loanid>\d+).+"amount":(?P<amount>[^,]+),"interest":(?P<interest>[0-9.]+),"months":(?P<months>\d+).*"finishedRatio":(?P<finish_ratio>[^,]+).*"nickName":"(?P<user_name>[^"]+).+"borrowerLevel":"(?P<risk_level>[^"]+)","address":"(?P<address>[^"]*)","jobType":"(?P<job_type>[^"]*)"',
+    re.S | re.M)
+
+
+# 定义爬虫函数
 def parse(url):
-    req = urllib2.Request(url, None, {'User-Agent': 'Mozilla/5.0'})  #pretend to be a browser
+    req = urllib2.Request(url, None, {'User-Agent': 'Mozilla/5.0'})  # pretend to be a browser
     try:
         html = urllib2.urlopen(req).read()
-        return [ m.groupdict() for m in pattern.finditer(html)]
+        return [m.groupdict() for m in pattern.finditer(html)]
     except:
-        return None    
-         
-#自定义爬虫起始点
-page_start = input('Please input the number you want to start:')
-page_end = input('Then input the number you want to stop:')+1
+        return None
 
-#导入数据库
+# 自定义爬虫起始点
+page_start = input('Please input the number you want to start:')
+page_end = input('Then input the number you want to stop:') + 1
+
+# 导入数据库
 index = 0
 try:
-    for page_index in range (page_start,page_end):
-        sName = '%d' %page_index
-        surl = r'http://www.we.com/lend/detailPage.action?loanId='+sName
+    for page_index in range(page_start, page_end):
+        sName = '%d' % page_index
+        surl = r'http://www.we.com/lend/detailPage.action?loanId=' + sName
         print sName
         x = parse(surl)
         if x != None and len(x) != 0:
             index = index + 1
-            cursor.execute('INSERT INTO loanlog (loanid, user_name, address, job_type, amount, interest, months, risk_level, finish_ratio ) VALUES (?,?,?,?,?,?,?,?,?)', (x[0]['loanid'], x[0]['user_name'].decode("utf8"), x[0]['address'].decode("utf8"), x[0]['job_type'].decode("utf8"), x[0]['amount'], x[0]['interest'], x[0]['months'], x[0]['risk_level'], x[0]['finish_ratio']))        
+            cursor.execute(
+                'INSERT INTO loanlog (loanid, user_name, address, job_type, amount, interest, months, risk_level, finish_ratio ) VALUES (?,?,?,?,?,?,?,?,?)',
+                (x[0]['loanid'], x[0]['user_name'].decode("utf8"), x[0]['address'].decode("utf8"),
+                 x[0]['job_type'].decode("utf8"), x[0]['amount'], x[0]['interest'], x[0]['months'], x[0]['risk_level'],
+                 x[0]['finish_ratio']))
             if index == 50:
                 index = 0
                 WE_db.commit()
-                print '50 records has been submitted.'       
+                print '50 records has been submitted.'
     WE_db.commit()
     print 'jobs done!'
-    
-except:
-    print 'there is an error at'+sName
 
-os.system("pause")
+except:
+    print 'there is an error at' + sName
+
+try:
+    input(u'按回车键退出')
+except:
+    pass
